0d2496952f9313cb360bbbd74ee010b74ad7c512
Danilo88901 <danya.kobal123456@gmail.com>
2026-06-01 01:08:45 +0200

    Update time_integration.cu

--- src/time_integration.cu
+++ src/time_integration.cu
@@ -19,6 +19,32 @@ __global__ void ssprk2_stage2(int height, int width, int length, double* phi_tmp
 		0.5 * phi[idx]
 		+ 0.5 * (phi_tmp[idx] + dt * rhs_tmp[idx]);
 }
+
+
+void ssprk2_stage1_cpu(int height, int width, int lentgh, int lz, int ly, int  lx, double* phi_tmp, double* rhs_phi, double* phi, double dt) {
+	for (int i = 2; i < height + 2; i++) {
+		for (int j = 2; j < width + 2; j++) {
+			for (int k = 2; k < lentgh + 2; k++) {
+				int idx = i * ly * lx + j * lx + k;
+				phi_tmp[idx] = phi[idx] + dt * rhs_phi[idx];
+			}
+		}
+	}
+}
+void ssprk2_stage2_cpu(int height, int width, int length, int lz, int ly, int lx, double* phi_tmp,
+	double* rhs_tmp, double* phi, double dt) {
+	for (int i = 2; i < height + 2; i++) {
+		for (int j = 2; j < width + 2; j++) {
+			for (int k = 2; k < length + 2; k++) {
+				int idx = i * ly * lx + j * lx + k;
+				phi[idx] =
+					0.5 * phi[idx]
+					+ 0.5 * (phi_tmp[idx] + dt * rhs_tmp[idx]);
+			}
+		}
+	}
+}
+
 double compute_dt(int height, int width, int length,
 	double* u, double* v, double* w,
 	double dx, double dy, double dz,
